8d87da33ab4bb16a4b4c08943802d40bc9517ba9
debian <debian@debian>
2021-09-24 12:16:39 -0400

    debut aleatoire

--- hyrule_castle.sh
+++ hyrule_castle.sh
@@ -12,6 +12,14 @@ str_e=5
 
 floor=1
 
+rarete=$(( $RANDOM % 101 ))
+csv=$1
+
+
+
+
+
+
 function ecran() {
     echo ====== FIGHT $floor ======
     echo $ennemi
@@ -66,22 +74,59 @@ function combat() {
 	
 }
 
-while [[ $floor -le 10 ]]
+function aleatoire(){ #accéder au fichier csv pour faire l'aleatoire
+    while IFS="," read -r id name hp mp str int def res spd luck race class rarity
+    do
+	if [[ ($rarete -ge 0) && ($rarete -le 50) ]];then #pour une rareté de 1
+	    if [[ $rarity == 1 ]];then
+		echo $name $rarity
+	    fi
+	elif [[ ($rarete -gt 50) && ($rarete -le 80) ]];then #pour une de 2 ...
+	    if [[ $rarity == 2 ]];then
+	        echo $name $rarity
+	    fi
+	elif [[ ($rarete -gt 80) && ($rarete -le 95) ]];then
+	    if [[ $rarity == 3 ]];then
+ 	        echo $name $rarity==3
+	    fi
+	    
+	elif [[ ($rarete -gt 95) && ($rarete -le 99) ]];then
+	    if [[ $rarity == 4 ]];then
+		echo $name $rarity==4
+	    fi
+	    
+	elif [[ ($rarete -gt 99) && ($rarete -le 100) ]];then
+	    if [[ $rarity == 5 ]];then
+		echo $name $rarity==5
+	    fi
+	    
+	fi
+    done < $1
+}
+
+aleatoire enemies.csv
+
+
+
+
+
+while [[ $floor -le 10 ]] #le jeu durera jusqu'a l'etage 10 atteint
 do
-    if [[ $floor -lt 10 ]];then
-	combat
-	hp_e=$hp_em
-	floor=$(($floor + 1))
-    elif [[ $floor -eq 10 ]];then
+    if [[ $floor -lt 10 ]];then #les 9 étages avant le boss 
+	combat #la fonction combat est appellé
+	hp_e=$hp_em #soigné la variable vie du monstre pour ne pas sauter des etages
+	floor=$(($floor + 1))#augmenter d'etages
+    elif [[ $floor -eq 10 ]];then #le combat contre le boss
 	ennemi="Ganon"
-	hp_e=150
-	str_e=20
+	hp_e=150 #changer la vie pour celle du boss
+	str_e=20 #changer la force ...
 	combat
-	floor=$(($floor + 1))
+	floor=$(($floor + 1))#monté d'un étage pour finir le jeu
     fi
 done
-if [[ $hp_e -le 0]];then
+if [[ $hp_e -le 0]];then #message de victoire
     echo "Princesse Zelda: Bien joué link! Tu as sauvé Hyrule! Tu es notre heros!"
-elif [[ $hp_p -le 0 ]];then
+elif [[ $hp_p -le 0 ]];then #message de defaite face a ganon
     echo "Princesse Zelda: Tu n'as pas reussi à temps Link... notre monde est perdue..."
+    echo "Vous voyez Ganon s'emparer de la tri force et rire face a sa victoire pendant que vous perdez concience"
 fi
